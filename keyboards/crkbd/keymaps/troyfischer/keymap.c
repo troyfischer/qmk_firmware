cc7c6f7311a3570b470e5095b9aff2961544ec76
troy <troytfischer@gmail.com>
2023-04-16 22:55:04 -0500

    removed all of the custom tap dance stuff
    QMK seems to have everything I need built in with much better control
    over key rolling

--- keyboards/crkbd/keymaps/troyfischer/keymap.c
+++ keyboards/crkbd/keymaps/troyfischer/keymap.c
@@ -19,77 +19,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-enum {
-    A_LSFT,
-    SEMI_RSFT,
-    J_GUI,
-    F_ALT,
-};
-
-typedef struct {
-    uint16_t tap;
-    uint16_t hold;
-    uint16_t held;
-} tap_dance_tap_hold_t;
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    tap_dance_action_t *action;
-
-    switch (keycode) {
-        case TD(A_LSFT):  // list all tap dance keycodes with tap-hold configurations
-        case TD(SEMI_RSFT):
-        case TD(J_GUI):
-        case TD(F_ALT):
-            action = &tap_dance_actions[TD_INDEX(keycode)];
-            if (!record->event.pressed && action->state.count && !action->state.finished) {
-                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
-                tap_code16(tap_hold->tap);
-            }
-    }
-    return true;
-}
-
-void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (state->pressed) {
-        if (state->count == 1
-#ifndef PERMISSIVE_HOLD
-            && !state->interrupted
-#endif
-        ) {
-            register_code16(tap_hold->hold);
-            tap_hold->held = tap_hold->hold;
-        } else {
-            register_code16(tap_hold->tap);
-            tap_hold->held = tap_hold->tap;
-        }
-    }
-}
-
-void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (tap_hold->held) {
-        unregister_code16(tap_hold->held);
-        tap_hold->held = 0;
-    }
-}
-
-#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
-    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   LT(3, KC_P),  KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO, TD(A_LSFT), LT(1, KC_S), KC_D, TD(F_ALT), KC_G,                     KC_H, TD(J_GUI), KC_K, LT(2, KC_L), TD(SEMI_RSFT), KC_NO,
+      KC_NO, SFT_T(KC_A), LT(1, KC_S), KC_D, ALT_T(KC_F), KC_G,                  KC_H, GUI_T(KC_J), KC_K, LT(2, KC_L), SFT_T(KC_SCLN), KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_BSPC, KC_SPC, KC_LCTL,     KC_RCTL,   KC_ENT, KC_TAB
+                                          KC_BSPC, KC_SPC, KC_LCTL,     KC_RCTL,   KC_ENT, MEH_T(KC_TAB)
                                       //`--------------------------'  `--------------------------
   ),
 
@@ -97,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_NO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_MINS, KC_7, KC_8, KC_9, KC_EQL, KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_NO, KC_LSFT, XXXXXXX, XXXXXXX, KC_ESC, XXXXXXX,                      KC_BSLS, KC_4, KC_5, KC_6, KC_0, KC_NO,
+       KC_NO, KC_LSFT, XXXXXXX, XXXXXXX, KC_ESC, KC_QUOT,                      KC_BSLS, KC_4, KC_5, KC_6, KC_0, KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_NO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_GRV, KC_1, KC_2, KC_3, KC_SLSH, KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -119,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -130,13 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-
 // Associate our tap dance key with its functionality
 tap_dance_action_t tap_dance_actions[] = {
-  [A_LSFT] = ACTION_TAP_DANCE_TAP_HOLD(KC_A, KC_LSFT),
-  [SEMI_RSFT] = ACTION_TAP_DANCE_TAP_HOLD(KC_SCLN, KC_RSFT),
-  [J_GUI] = ACTION_TAP_DANCE_TAP_HOLD(KC_J, KC_RGUI),
-  [F_ALT] = ACTION_TAP_DANCE_TAP_HOLD(KC_F, KC_RALT),
 };
 
 #ifdef OLED_ENABLE
